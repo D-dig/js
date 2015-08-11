60bc1a45d5a618291ec8f1f407623c778a97903d
Ardilas Baidowi <thesonicman2@yahoo.co.id>
2015-08-11 08:49:46 +0700

    Update sitemap3.js

--- sitemap3.js
+++ sitemap3.js
@@ -1,3 +1,4 @@
+
 var numposts = 100;
 var standardstyling = true;
 /*programmer and maker of this code is belong to founder of http://www.ardilas.com*/
@@ -23,8 +24,4 @@ function startpost(json){
   }
   
   if (standardstyling) document.write('</li>');
-}
-
-function finished(){
-  document.write('<br /><br /><a href="http://www.ardilas.com/2015/08/cara-membuat-bikin-daftar-isi-sitemap-blogger-blogspot-dengan-berdasarkan-label-kategori-topik-tertentu.html" style="font-size: 10 px; text-decoration:none; color: #616469;" target="_blank" title="Cara Ke-3 Buat Daftar Isi Dengan Label Tertentu di Blogspot">Sitemap</a>');
 }
